0ed1055425d2d7479c289dfd9b2eb987aa9133b8
MD-HABIB-ULLA <habibulla1278@gmail.com>
2024-06-25 23:42:11 +0600

    route

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navber from "./Components/Navber/Navber";
+import Navber from "@/Components/Navber/Navber";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
